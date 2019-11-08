d37fbd1b3b1b3b07c2288ffae58cfb0a09a91a74
Demid Ost <demostapenko@gmail.com>
2019-11-08 14:20:45 +0200

    Updates

---
+++ precache-manifest.77fe76bdab1596c39c3551531cd6a811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d494b7f0928401dadf3d4eb1857624d",
+    "url": "/coingaming-test/index.html"
+  },
+  {
+    "revision": "d5bc7fe88ea77d017121",
+    "url": "/coingaming-test/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "e085a6987da6848353d3",
+    "url": "/coingaming-test/static/js/2.224976bc.chunk.js"
+  },
+  {
+    "revision": "d5bc7fe88ea77d017121",
+    "url": "/coingaming-test/static/js/main.7c6a730b.chunk.js"
+  },
+  {
+    "revision": "7465907530da101796cf",
+    "url": "/coingaming-test/static/js/runtime-main.1980cca7.js"
+  }
+]);
